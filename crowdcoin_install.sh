6a16a4fee32871e65a9a106b92cbb3ccc1a26b5b
ronaldr1987 <36152948+ronaldr1987@users.noreply.github.com>
2018-06-15 14:27:17 +0200

    Update crowdcoin_install.sh

--- crowdcoin_install.sh
+++ crowdcoin_install.sh
@@ -287,7 +287,7 @@ function important_information() {
  echo -e "${PURPLE}Windows Wallet Guide. https://github.com/ronaldr1987/crowdcoin/blob/master/README.md${NC}"
  echo -e "${BLUE}================================================================================================================================${NC}"
  echo -e "${GREEN}$COIN_NAME masternode is up and running and listening on port ${NC}${PURPLE}$COIN_PORT${NC}."
- echo -e 
+ echo -e "${BLUE}${NC}"
  echo -e "${GREEN}Start: ${NC}${RED}systemctl start $COIN_NAME.service${NC}"
  echo -e "${GREEN}Stop: ${NC}${RED}systemctl stop $COIN_NAME.service${NC}"
  echo -e "${GREEN}Status: ${NC}${RED}systemctl status $COIN_NAME.service${NC}"
@@ -295,11 +295,9 @@ function important_information() {
  echo -e "${GREEN}MASTERNODE GENKEY: ${NC}${PURPLE}$COINKEY${NC}"
  echo -e "${GREEN}Configuration file: ${NC}${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
  if [[ -n $SENTINEL_REPO  ]]; then
- echo -e "Sentinel folder: ${RED}$CONFIGFOLDER/sentinelLinux${NC}"
- echo -e "Sentinel logfile: ${RED}$CONFIGFOLDER/sentinel.log${NC}"
+ echo -e "${GREEN}Sentinel folder: ${NC}${RED}$CONFIGFOLDER/sentinelLinux${NC}"
+ echo -e "${GREEN}Sentinel logfile: ${NC}${RED}$CONFIGFOLDER/sentinel.log${NC}"
  fi
- echo -e "${BLUE}================================================================================================================================"
- echo -e "${CYAN}Follow twitter to stay updated.  https://twitter.com/${NC}"
  echo -e "${BLUE}================================================================================================================================${NC}"
  echo -e "${CYAN}Ensure Node is fully SYNCED with BLOCKCHAIN.${NC}"
  echo -e "${BLUE}================================================================================================================================${NC}"

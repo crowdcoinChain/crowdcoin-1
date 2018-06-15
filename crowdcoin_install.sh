dbb2408e3489613070e03ef42ef9932215ceef63
ronaldr1987 <36152948+ronaldr1987@users.noreply.github.com>
2018-06-15 14:33:36 +0200

    Update crowdcoin_install.sh

--- crowdcoin_install.sh
+++ crowdcoin_install.sh
@@ -140,7 +140,7 @@ cd $CONFIGFOLDER
 }
 
 function create_key() {
-  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}."
+  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME masternode genkey${NC}."
   read -e COINKEY
   if [[ -z "$COINKEY" ]]; then
   $COIN_PATH$COIN_DAEMON -daemon
